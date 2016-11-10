abd4973194360df3892099d861aa1acbe59520cb
Andrew Chen <mochination@gmail.com>
2016-11-10 09:00:06 -0800

    Sound fixes

--- js/rainplayer.js
+++ js/rainplayer.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
   rainBeijing = new Howl({
-    volume: 0.7,
+    volume: 0.25,
     loop: true,
     src: ['./sound/rain-beijing.ogg']
   });
 
   rainNakano = new Howl({
-    volume: 0.1,
+    volume: 0.05,
     loop: true,
     src: ['./sound/rain-nakano.ogg']
   });
 
   rainEiko = new Howl({
-    volume: 0.1,
+    volume: 0.05,
     loop: true,
     src: ['./sound/rain-night-eiko-temple.ogg']
   });
@@ -21,24 +21,23 @@ $(document).ready(function () {
   pauseBtn.style.display = 'inline';
 
   $( "#playBtn" ).click(function() {
-    rainBeijing.fade(0, 0.7, 1000)
-    rainNakano.fade(0, 0.1, 1000)
-    rainEiko.fade(0, 0.1, 1000)
+    rainBeijing.fade(0, 0.25, 1000)
+    rainNakano.fade(0, 0.05, 1000)
+    rainEiko.fade(0, 0.05, 1000)
     playBtn.style.display = 'none';
     pauseBtn.style.display = 'inline';
   });
 
 
   $( "#pauseBtn" ).click(function() {
-    rainBeijing.fade(0.7, 0, 1000)
-    rainNakano.fade(0.1, 0, 1000)
-    rainEiko.fade(0.1, 0, 1000)
+    rainBeijing.fade(0.25, 0, 1000)
+    rainNakano.fade(0.05, 0, 1000)
+    rainEiko.fade(0.05, 0, 1000)
     playBtn.style.display = 'inline';
     pauseBtn.style.display = 'none';
   });
-
+  
   rainBeijing.play()
   rainNakano.play()
   rainEiko.play()
-
 })
