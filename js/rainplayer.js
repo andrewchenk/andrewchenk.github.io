664d951aad14b728e51bc066f13570f6b8859e02
zoutachi <mochination@gmail.com>
2016-12-30 16:18:34 -0800

    made it so that rain doesn't play until you turn it on
    I have sacrificed my principles

--- js/rainplayer.js
+++ js/rainplayer.js
@@ -1,27 +1,27 @@
 $(document).ready(function () {
-  rain = new Howl({
-    volume: 0.25,
-    loop: true,
-    src: ['./sound/rain-bjeknkn.ogg']
-  });
+    rain = new Howl({
+        volume: 0.25,
+        loop: true,
+        src: ['./sound/rain-bjeknkn.ogg']
+    });
 
-  playBtn.style.display = 'none';
-  pauseBtn.style.display = 'inline';
-
-  $( "#playBtn" ).click(function() {
-    rain.fade(0, 0.25, 1000)
-    playBtn.style.display = 'none';
-    pauseBtn.style.display = 'inline';
-  });
-
-
-  $( "#pauseBtn" ).click(function() {
-    rain.fade(0.25, 0, 1000)
     playBtn.style.display = 'inline';
     pauseBtn.style.display = 'none';
-  });
 
-  rain.play()
-  rain.fade(0, rain.volume(), 500)
+    $("#playBtn").click(function () {
+        rain.play()
+        rain.fade(0, 0.15, 1000)
+        playBtn.style.display = 'none';
+        pauseBtn.style.display = 'inline';
+    });
+
+
+    $("#pauseBtn").click(function () {
+        rain.fade(0.15, 0, 1000)
+        playBtn.style.display = 'inline';
+        pauseBtn.style.display = 'none';
+    });
+
+
 
 })
