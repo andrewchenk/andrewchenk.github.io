6fab23d0c485cc05ed8d4b901af5e60d4a95544b
Andrew Chen <mochination@gmail.com>
2017-11-20 01:47:29 -0800

    Fixed link bug, content and style updates, and rainplayer
    Previously hovering over links would cause a small but visible jump.
    Fixed somehow.
    Rainplayer doesn't play more and more instances of rain in the sound
    module.

--- js/rainplayer.js
+++ js/rainplayer.js
@@ -1,23 +1,27 @@
 $(document).ready(function () {
     rain = new Howl({
-        volume: 0.25,
         loop: true,
         src: ['./sound/rain-bjeknkn.ogg']
     });
+    var rain1;
 
     playBtn.style.display = 'inline';
     pauseBtn.style.display = 'none';
 
     $("#playBtn").click(function () {
-        rain.play()
-        rain.fade(0, 0.15, 1000)
+
+        rain1 = rain.play()
+        rain.fade(0, 0.09, 1000, rain1);
         playBtn.style.display = 'none';
         pauseBtn.style.display = 'inline';
     });
 
 
     $("#pauseBtn").click(function () {
-        rain.fade(0.15, 0, 1000)
+        rain.fade(0.09, 0, 1000, rain1);
+        if (rain.volume() == 0) {
+            rain1 = rain.pause();
+          }
         playBtn.style.display = 'inline';
         pauseBtn.style.display = 'none';
     });
